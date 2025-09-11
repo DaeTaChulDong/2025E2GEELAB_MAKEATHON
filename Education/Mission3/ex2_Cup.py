bef18940f38909c1421668681757c0172faafb26
대타출동 <166669923+DaeTaChulDong@users.noreply.github.com>
2025-09-11 13:56:27 +0900

    Update ex2_Cup.py

--- Education/Mission3/ex2_Cup.py
+++ Education/Mission3/ex2_Cup.py
@@ -12,11 +12,12 @@ jk.lcd_set(0x27,16,2)
 jk.neopixel_set(NEO,4)
 jk.neopixel_bright(NEO,20)
 
-while():
+while True:
 	light=jk.cds_read(CDS)
 	jk.lcd_display(0,0,str(light))
 	
 	if (200<light) & (light<600):
 		jk.neopixel_display_all(NEO,0,0,255)
 	time.sleep(1)
+
 	jk.neopixel_clear(NEO)
