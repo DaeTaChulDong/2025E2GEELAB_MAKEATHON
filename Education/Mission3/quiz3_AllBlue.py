0e04d1efb2c36efcde7fcf8d5c771a69a53c11f9
대타출동 <166669923+DaeTaChulDong@users.noreply.github.com>
2025-09-11 13:57:52 +0900

    Update quiz3_AllBlue.py

--- Education/Mission3/quiz3_AllBlue.py
+++ Education/Mission3/quiz3_AllBlue.py
@@ -10,8 +10,9 @@ jk.start()
 jk.neopixel_set(NEO,4)
 jk.neopixel_bright(NEO,20)
 
-while():
+while True:
 	jk.neopixel_display_all(Neo,0,0,255)
 	time.sleep(1)
 	jk.neopixel_clear(NEO)
+
 	time.sleep(1)
