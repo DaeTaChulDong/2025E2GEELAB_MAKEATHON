a204bc3e2ab298ba67b8ee082ceceb61412c614a
대타출동 <166669923+DaeTaChulDong@users.noreply.github.com>
2025-09-11 13:57:22 +0900

    Update quiz12_MultiOutput.py

--- Education/Mission3/quiz12_MultiOutput.py
+++ Education/Mission3/quiz12_MultiOutput.py
@@ -15,11 +15,12 @@ jk.neopixel_set(NEO,4)
 jk.neopixel_bright(NEO,20)
 jk.lcd_set(0x27,16,2)
 
-while():
+while True:
 	lux=jk.cds_read(CDS)
 	jk.lcd_display(0,0,str(lux))
 	if lux>500:
 		jk.mp3_play(1)
 		jk.neopixel_display_all(NEO,255,0,0)
 		time.sleep(2)
+
 	jk.neopixel_clear(NEO)
