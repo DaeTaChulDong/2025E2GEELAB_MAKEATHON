e4df4c03ff32416db21caf83c3d77ad25b001082
대타출동 <166669923+DaeTaChulDong@users.noreply.github.com>
2025-09-11 13:55:29 +0900

    Update quiz2_Buzzer.py

--- Education/Mission2/quiz2_Buzzer.py
+++ Education/Mission2/quiz2_Buzzer.py
@@ -11,7 +11,7 @@ jk.serial_connect(PORT)
 jk.start()
 
 #부저 울리기(pin,tone,type): pin 개별적으로 바꾸기
-while():
+while True:
 	jk.buzzer(BUZZER,131,1)
 	time.sleep(1)
 	jk.buzzer(BUZZER,147,1)
@@ -19,4 +19,5 @@ while():
 	jk.buzzer(BUZZER,165,1)
 	time.sleep(1)
 	jk.buzzer(BUZZER,175,1)
+
 	time.sleep(1)
