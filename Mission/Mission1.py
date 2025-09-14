39f87ba0367e5ce90845f11c7c3c9dcfb787c13b
대타출동 <166669923+DaeTaChulDong@users.noreply.github.com>
2025-09-15 00:07:00 +0900

    Add files via upload

---
+++ Mission/Mission1.py
@@ -0,0 +1,42 @@
+from jikko.jikko import *
+jk=Pyjikko()
+
+PORT='COM4'
+RX=10
+TX=11
+NEO=7
+TRIG=13
+ECHO=12
+
+jk.serial_connect(PORT)
+jk.start()
+
+jk.mp3_set(RX,TX)
+jk.mp3_volume(20)
+jk.lcd_set(0x27,16,2)
+jk.neopixel_set(NEO,4)
+jk.neopixel_bright(NEO,20)
+
+while True:
+	distance=jk.sonic_read(TRIG,ECHO)
+	distance_map=jk.map_value(distance,0,50,0,100)
+	jk.lcd_display(0,0,str(distance))
+	jk.lcd_display(0,1,str(distance_map))
+	jk.neopixel_bright(NEO,distance_map)
+	
+	if 0<=distance<=10:
+		jk.neopixel_display_all(NEO,255,0,0)
+		jk.mp3_play(1)
+	elif 10<=distance<=20:
+		jk.neopixel_display_all(NEO,255,255,0)
+		jk.mp3_play(2)
+	elif 20<=distance<=30:
+		jk.neopixel_display_all(NEO,0,0,255)
+		jk.mp3_play(3)
+	elif 30<=distance<=40:
+		jk.neopixel_display_all(NEO,0,255,0)
+		jk.mp3_play(4)
+	else:
+		jk.neopixel_clear(NEO)
+	
+	jk.lcd_clear()
---
+++ Mission/Mission2.py
@@ -0,0 +1,35 @@
+from jikko.jikko import *
+jk=Pyjikko()
+
+PORT='COM4'
+NEO=7
+TEM_HUM=4
+
+jk.serial_connect(PORT)
+jk.start()
+
+jk.lcd_set(0x27,16,2)
+jk.neopixel_set(NEO,4)
+jk.neopixel_bright(NEO,20)
+
+while True:
+	T=jk.temp_read(TEM_HUM)
+	H=jk.humi_read(TEM_HUM)
+	jk.lcd_display(0,0,str(T))
+	jk.lcd_display(0,1,str(H))
+	
+	if 40<H<60:
+		jk.neopixel_display(NEO,0,0,0,255)
+	elif 60<H<80:
+		jk.neopixel_display(NEO,0,0,0,255)
+		jk.neopixel_display(NEO,1,0,0,255)
+	elif 80<H<90:
+		jk.neopixel_display(NEO,0,0,0,255)
+		jk.neopixel_display(NEO,1,0,0,255)
+		jk.neopixel_display(NEO,2,0,0,255)
+	elif 90<H<96:
+		jk.neopixel_display_all(NEO,0,0,255)
+	
+	time.sleep(1)
+	jk.neopixel_clear(NEO)
+	jk.lcd_clear()
