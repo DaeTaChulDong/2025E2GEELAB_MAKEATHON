6a496ae2569a0e648e3056a771ab823957680e8a
대타출동 <166669923+DaeTaChulDong@users.noreply.github.com>
2025-09-15 00:05:41 +0900

    Add files via upload

---
+++ Mission/mission2.py
@@ -0,0 +1,30 @@
+from jikko.jikko import *
+
+jk=Pyjikko()
+
+PORT='COM4'
+SERVO=8
+TRIG=13
+ECHO=12
+
+jk.serial_connect(PORT)
+jk.start()
+
+jk.lcd_set(0x27,16,2)
+
+while True:
+	distance=jk.sonic_read(TRIG,ECHO)
+	jk.lcd_display(0,0,str(distance))
+	time.sleep(1)
+	
+	if distance<20:
+		jk.servo_degree(SERVO,90)
+		jk.lcd_display(0,1,"open")
+		time.sleep(2)
+	
+	if 20<distance:
+		jk.servo_degree(SERVO,0)
+		jk.lcd_display(0,1,"close")
+		time.sleep(1)
+	jk.lcd_clear()
+
---
+++ Mission/misson1.py
@@ -0,0 +1,24 @@
+from jikko.jikko import *
+
+jk=Pyjikko()
+
+PORT='COM4'
+
+BUZZER=6
+LED=5
+
+jk.serial_connect(PORT)
+jk.start()
+
+while True:
+	jk.led_digital(LED,HIGH)
+	jk.buzzer(BUZZER, 131,1)
+	jk.buzzer(BUZZER, 147,1)
+	jk.buzzer(BUZZER, 165,1)
+	jk.buzzer(BUZZER, 175,1)
+	jk.buzzer(BUZZER,196,1)
+	
+	jk.led_digital(LED,LOW)
+	time.sleep(3)
+	
+
